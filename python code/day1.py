99c4063373ef0e9d403096098ae33407846d46e6
Seungmin Jeong <sontneptm2@gmail.com>
2022-03-14 16:12:54 +0900

    update

--- python code/day1.py
+++ python code/day1.py
@@ -1,3 +1,4 @@
+from statistics import mean
 import numpy as np
 import json
 from PIL import Image
@@ -19,11 +20,32 @@ class BaseTransform():
     def __call__(self, img):
         return self.base_transform(img)
 
+class ILSVRCPredictor():
+    def __init__(self, class_index) -> None:
+        self.class_index = class_index
+
+    def predict_max(self, out):
+        maxid = np.argmax(out.detach().numpy())
+        predicted_label_name = self.class_index[str(maxid)][1]
+
+        return predicted_label_name
 
 image_path = 'exam_source/1_image_classification/data/goldenretriever-3724972_640.jpg'
 img = Image.open(image_path)
 
 plt.imshow(img)
+plt.show()
+
+resize = 224
+mean = (0.485, 0.456, 0.406)
+std = (0.229, 0.224, 0.225)
+transform = BaseTransform(resize, mean, std)
+img_transformed = transform(img)
+
+img_transformed = img_transformed.numpy().transpose((1,2,0))
+img_transformed = np.clip(img_transformed,0,1)
+
+plt.imshow(img_transformed)
 plt.show()
 
 # use_pretrained = True
