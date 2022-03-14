d385f4143288dfa2ae8450e6b9f37f93002b227b
Seungmin Jeong <sontneptm2@gmail.com>
2022-03-14 16:38:37 +0900

    update

---
+++ python code/day1.py
@@ -0,0 +1,12 @@
+import matplotlib
+import numpy as np
+import json
+from PIL import Image
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+from torchvision import models, transforms
+
+print(torch.__version__)
+print(torchvision.__version__)
+
